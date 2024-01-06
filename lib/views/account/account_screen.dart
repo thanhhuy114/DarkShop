67913fb779a425229573ddd2c02f00fca3bca965
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-06 22:21:10 +0700

    tạo file chứa màn hình

--- lib/views/account/account_screen.dart
+++ lib/views/account/account_screen.dart
@@ -1,1 +1,17 @@
-//giao diện tài khoản
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatefulWidget {
+  const AccountScreen({super.key});
+
+  @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
