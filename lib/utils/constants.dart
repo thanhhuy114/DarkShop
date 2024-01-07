aefc3f9d98d83c55f2ec5197784025778110269b
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-07 16:07:54 +0700

    thêm các hằng - loại thông báo

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,19 @@
-//chứa các hằng
+enum NotificationType {
+  orderSuccessful,
+  newOrder,
+  paymentSuccessful,
+  deliverySuccessful,
+  orderCancelled,
+  outOfStock,
+}
+
+class Constants {
+  static const Map<NotificationType, String> notificationTypes = {
+    NotificationType.orderSuccessful: "Đặt hàng thành công",
+    NotificationType.newOrder: "Đơn hàng mới",
+    NotificationType.paymentSuccessful: "Thanh toán thành công",
+    NotificationType.deliverySuccessful: "Giao hàng thành công",
+    NotificationType.orderCancelled: "Đơn hàng đã hủy",
+    NotificationType.outOfStock: "Sắp hết hàng",
+  };
+}
