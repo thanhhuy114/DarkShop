6c3c98a7b5b7ae1e55e47fc25c0b9ad3c4f199ba
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-07 16:00:39 +0700

    xong giao diện 1 thông báo

---
+++ lib/views/notification/components/notification_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/models/notification.dart';
+import '../../../utils/colors.dart';
+import '../notification_presenter.dart';
+
+class NotificationCard extends StatelessWidget {
+  const NotificationCard({Key? key, required this.notification})
+      : super(key: key);
+
+  final NotificationInfo notification;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child: GestureDetector(
+        onTap: () => NotificationPresenter().onclickCard(notification.idObject, notification.type),
+        child: Container(
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: MyColors.backgroundCard,
+          ),
+          child: Row(
+            children: [
+              const Icon(Icons.notifications, size: 80),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(right: 10, top: 5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            if (!notification.read)
+                              const Icon(
+                                Icons.circle,
+                                size: 8.0,
+                                color: Colors.blue,
+                              ),
+                            Text(
+                              NotificationPresenter()
+                                  .getNotificationTime(notification.date),
+                              style: const TextStyle(fontSize: 10),
+                            )
+                          ],
+                        ),
+                      ),
+                      Text(
+                        NotificationPresenter()
+                            .getNotificationTitle(notification.type),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        notification.description,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
