49c80eedb08f83f61db489de2d69f25c393e769a
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-08 10:20:08 +0700

    viết các hàm mô phỏng lấy dữ liệu

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -1,8 +1,76 @@
-import 'package:darkshop/data/models/user.dart';
+import 'package:flutter/services.dart';
 
-class UserRepository{
-  static User? userLogin;
+import '../models/user.dart';
 
-  //các hàm
+class UserRepository {
+  Future<User?> getUserLogin() async {
+    return User(
+        id: 1,
+        isAdmin: true,
+        fullname: "Trần Dương Gia Thịnh",
+        image: null,
+        phone: "0987654321",
+        email: "tdgt@gmail.com",
+        recentAddress: "HCM",
+        blocked: false);
+  }
 
+  Future<List<User>?> getAllUsers() async {
+    return [
+      User(
+          id: 1,
+          isAdmin: true,
+          fullname: "Trần Dương Gia Thịnh",
+          image: null,
+          phone: "0987654321",
+          email: "tdgt@gmail.com",
+          recentAddress: "HCM",
+          blocked: false),
+      User(
+          id: 2,
+          isAdmin: false,
+          fullname: "Võ Thành Huy",
+          image: null,
+          phone: "0123456789",
+          email: "vth@gmail.com",
+          recentAddress: "HN",
+          blocked: false)
+    ];
+  }
+  
+  saveAvatar(Uint8List image, int idUser) async {
+    //lưu avatar mới vào database
+  }
+  
+  saveFullname(String fullname, int idUser) async {
+    
+  }
+  
+  saveEmail(String email, int idUser) async {
+    
+  }
+  
+  savePhone(String phone, int idUser) async {
+    
+  }
+  
+  addAdress(String newAddress, int idUser) async {
+    
+  }
+  
+  updateAdress(String newAddress, int idAddress, int idUser) async {
+    
+  }
+  
+  deleteAdress(String newAddress, int idUser) async {
+    
+  }
+
+  changePassword(String newPassword, int idUser) async {
+    //đổi mất khẩu
+  }
+
+  logout() async {
+    //đăng xuất
+  }
 }
