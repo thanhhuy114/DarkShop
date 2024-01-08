41350b2a4f1fefbc3ea6c3cdd45b21442b5eb314
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-08 10:19:24 +0700

    thêm màu

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class MyColors{
   static Color background = Colors.orange;
-  static Color button = Colors.red;
+  static Color backgroundButton = Colors.red;
   static Color backgroundCard = Colors.red;
   static Color backgroundAppBar = Colors.red;
+  static Color backgroundUserInfo = Colors.white;
 }
