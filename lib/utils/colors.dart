95ae4f55635ae23843b0647517bec22438ed04f8
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-10 09:35:13 +0700

    thêm màu giá khuyến mãi

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MyColors{
-  static const Color background = Colors.orange;
+  static const Color backgroundApp = Colors.orange;
+  static const Color backgroundPromotion = Colors.orange;
+  static const Color promotionPrice = Colors.orange;
   static const Color backgroundButton = Colors.red;
   static const Color backgroundCard = Colors.red;
   static const Color backgroundAppBar = Colors.red;
