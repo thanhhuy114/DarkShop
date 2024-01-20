0c2590fd1053fe3649b631321a6ced7f1074394a
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-20 11:01:47 +0700

    thêm thông báo khi kong6 có internet

--- lib/views/notification/components/list_notifications.dart
+++ lib/views/notification/components/list_notifications.dart
@@ -1,3 +1,5 @@
+import 'package:darkshop/utils/colors.dart';
+import 'package:darkshop/utils/constants.dart';
 import 'package:darkshop/views/notification/components/notification_card.dart';
 import 'package:darkshop/views/notification/notification_presenter.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +14,23 @@ class ListNotifications extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: ListView(
         children: [
-          if(presenter.notifications.isEmpty)const  Text("Không có thông báo!", textAlign: TextAlign.center,),
+          if (!NotificationPresenter.connected)
+            Container(
+              height: 20,
+              width: MediaQuery.of(context).size.width - 16,
+              color: MyColors.backgoundRowMessage,
+              child: const Text(
+                Constants.noConnected,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          if (!NotificationPresenter.connected)
+            const Padding(padding: EdgeInsets.only(top: 5)),
+          if (presenter.notifications.isEmpty)
+            const Text(
+              "Không có thông báo!",
+              textAlign: TextAlign.center,
+            ),
           for (int i = 0; i < presenter.notifications.length; i++)
             NotificationCard(
               notification: presenter.notifications[i],
